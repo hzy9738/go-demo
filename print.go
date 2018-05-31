6d478f3df3390251eca95801326c8e53229793b1
hzy9738 <hzy9738@126.com>
2018-05-31 16:56:44 +0800

    Go demo
    Go Demo

---
+++ print.go
@@ -0,0 +1,33 @@
+package main;
+
+import "fmt"
+
+var (
+	aa = 3
+	bb = 3
+	cc = 3
+)
+
+
+func variableZeroValue() {
+	var i int          //默认0
+	var n string       //默认""
+	fmt.Printf("%d %q\n", i , n);    //%q可以把""打印出来
+}
+
+func variableInitiaValue()  {
+	var a ,b int
+	var c ,d string
+	f ,g := 3,"123"
+	var e = true
+	var h = "黄"
+	fmt.Println(a,b,c,d,f,g,e,h)
+}
+
+
+func main() {
+	fmt.Println("hello world")
+	variableZeroValue();
+	variableInitiaValue();
+	fmt.Println(aa,bb,cc)
+}
